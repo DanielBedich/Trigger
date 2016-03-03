5910441d10e9618e60649f39378054eb77ce123a
DanielBedich <bedich.2@osu.eduu>
2016-03-03 13:01:18 -0500

    Merge branch 'master' of /Users/DanielBedich/Desktop/Trigger with conflicts.

--- app/src/main/java/com/example/danielbedich/trigger/MainActivity.java
+++ app/src/main/java/com/example/danielbedich/trigger/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,12 +14,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Log.d("Tag", "Entering onCreate");
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -43,17 +40,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();  // Always call the superclass method first
-        Log.d("Tag", "Entering onPause");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();  // Always call the superclass method first
-        Log.d("Tag", "Entering onResume");
     }
 }
