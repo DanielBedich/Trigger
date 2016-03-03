79e2aad57494572d811d48687f3ca17c35b573fb
DanielBedich <bedich.2@osu.eduu>
2016-03-03 17:32:58 -0500

    Added onDestroy, onResume, onStop

--- app/src/main/java/com/example/danielbedich/trigger/MainActivity.java
+++ app/src/main/java/com/example/danielbedich/trigger/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.danielbedich.trigger;
 
 import android.os.Bundle;
@@ -5,19 +6,72 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String TEXT_BOX = "text_box";
+
+
+    private EditText mEditText;
+    private Spinner mSpinnerTrigger;
+    private Spinner mSpinnerAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mSpinnerTrigger = (Spinner) findViewById(R.id.trigger_spinner);
+        mSpinnerAction = (Spinner) findViewById(R.id.action_spinner);
+
+        mEditText = (EditText) findViewById(R.id.content_editText);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();  // Always call the superclass method first
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
@@ -41,4 +95,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
