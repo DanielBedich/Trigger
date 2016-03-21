017eef0f42edd0ef8e0361c409a74f1da443688d
DanielBedich <bedich.2@osu.eduu>
2016-03-21 16:27:14 -0400

    Added contact selection and sending text

--- app/src/main/java/com/example/danielbedich/trigger/CreateActivity.java
+++ app/src/main/java/com/example/danielbedich/trigger/CreateActivity.java
@@ -1,21 +1,36 @@
 package com.example.danielbedich.trigger;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class CreateActivity extends AppCompatActivity {
 
+    private static final int CONTACT_PICKER = 1;
+    private static final String DEBUG_TAG = null;
     private Button mButtonSave;
     private Button mButtonCancel;
+    private EditText mContactText;
+    private EditText mMessageText;
+    private String contactID;
+    private Uri uriContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        mMessageText = (EditText) findViewById(R.id.message_text);
 
         mButtonSave = (Button) findViewById(R.id.save_button);
         mButtonSave.setOnClickListener(new View.OnClickListener() {
@@ -32,10 +47,81 @@ public class CreateActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent cancelAction = new Intent(v.getContext(), TriggerActivity.class);
                 startActivity(cancelAction);
+
+                //How to text someone
+                SmsManager smsMan = SmsManager.getDefault();
+                String num = mContactText.getText().toString();
+                String mes= mMessageText.getText().toString();
+                smsMan.sendTextMessage(num,null,mes,null, null);
             }
         });
+
+        mContactText = (EditText) findViewById(R.id.contact_name);
+        mContactText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                startActivityForResult(intent, CONTACT_PICKER);
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CONTACT_PICKER && resultCode == RESULT_OK){
+
+            uriContact = data.getData();
+            getContactName();
+            getContactNumber();
+        }
+    }
+
+    private void getContactNumber(){
+        String contactNumber = null;
+
+        Cursor cursorID = getContentResolver().query(uriContact, new String[]{ContactsContract.Contacts._ID},null, null,null);
+
+        if(cursorID.moveToFirst()){
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
+        }
+
+        cursorID.close();
+
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID+" = ? AND "
+                + ContactsContract.CommonDataKinds.Phone.TYPE+" = "
+                + ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+                new String[]{contactID},null);
+
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        }
+        mContactText.setText(contactNumber);
+        cursorPhone.close();
+    }
+
+    private void getContactName() {
+
+        String contactName = null;
+
+        // querying contact data store
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            // DISPLAY_NAME = The display name for the contact.
+            // HAS_PHONE_NUMBER =   An indicator of whether this contact has at least one phone number.
+
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+        cursor.close();
+
+    }
 
 
 }
